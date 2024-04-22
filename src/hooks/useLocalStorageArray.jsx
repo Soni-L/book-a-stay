6d4729d3a7c97ded8eb608c757c2483aea3f42aa
soni <acdsoni@gmail.com>
2024-04-22 10:48:03 +0200

    useLocalStorageArray hook

---
+++ src/hooks/useLocalStorageArray.jsx
@@ -0,0 +1,44 @@
+import useLocalStorage from "use-local-storage";
+import { v4 as uuidv4 } from "uuid";
+
+const useLocalStorageArray = (key) => {
+  const [items, setItems] = useLocalStorage(key, []);
+
+  const addItem = (newItem) => {
+    const newItemWithId = { id: uuidv4(), ...newItem };
+    setItems([...items, newItemWithId]);
+  };
+
+  const getItems = () => {
+    return items;
+  };
+
+  const getItemById = (id) => {
+    return items.find((item) => item.id === id);
+  };
+
+  const deleteItem = (id) => {
+    const updatedItems = items.filter((item) => item.id !== id);
+    setItems(updatedItems);
+  };
+
+  const updateItem = (updatedItem) => {
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      }
+      return item;
+    });
+    setItems(updatedItems);
+  };
+
+  return {
+    addItem,
+    getItems,
+    getItemById,
+    deleteItem,
+    updateItem,
+  };
+};
+
+export default useLocalStorageArray;
